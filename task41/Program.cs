703a1a3f55dcd5c61f3af5c3c954aed95372d957
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-26 12:50:35 +0700

    task41

--- task41/Program.cs
+++ task41/Program.cs
@@ -2,3 +2,16 @@
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите необходимое количество чисел через пробел");
+int [] arr = Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+int res = 0;
+for(int i = 0;i<arr.Length;i++)
+{
+    if(arr[i]>0)
+    {
+        res++;
+    }
+}
+Console.WriteLine($"Количество чисел больше 0 равно {res}");
+
